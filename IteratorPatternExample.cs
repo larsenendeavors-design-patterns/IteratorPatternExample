38612a2af5d22bfbf4fb6f6343c295349adcc021
skathix <32309102+skathix@users.noreply.github.com>
2021-10-09 12:48:31 -0500

    Initial commit

---
+++ IteratorPatternExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IteratorPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
